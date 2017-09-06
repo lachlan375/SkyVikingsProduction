2e2a98e606df428fb46cdcb129313bbd8b7700d5
AIE\s171545 <lachlan1066@gmail.com>
2017-09-06 16:59:06 +0930

    commit with assets

--- Assets/SceneTransistion.cs
+++ Assets/SceneTransistion.cs
@@ -1,17 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneTransistion : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    public int index;
+    public string levelName;
 
+    public Image black;
+    public Animator anim;
 
-    SceneTransistionScript()
+    void SceneTransistionFunction()
     {
+
+        StartCoroutine(Fading());
+    }
+
+
+    IEnumerator Fading()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => black.color.a == 1);
+        SceneManager.LoadScene(index);
     }
 }
