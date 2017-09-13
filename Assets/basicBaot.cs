8de4876be716cac00a95946383b665866a1b52ee
AIE\s171138 <yerilth2@gmail.com>
2017-09-13 14:12:10 +0930

    basic boat ai

---
+++ Assets/basicBaot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class basicBaot : MonoBehaviour {
+    public Transform [] target;
+    public NavMeshAgent navM;
+    public int theTarget;
+    public hitBox alertField;
+    // Use this for initialization
+    void Start () {
+        theTarget = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        navM.SetDestination(target[theTarget].transform.position);
+        if(alertField.inTheBox == true)
+        {
+            theTarget = 1;
+        }
+        if (alertField.inTheBox == false)
+        {
+            theTarget = 0;
+        }
+    }
+}
