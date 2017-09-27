728a295fc65ea8c0b53e88f5a7dba393a6698ce8
AIE\s171138 <yerilth2@gmail.com>
2017-09-27 15:21:07 +0930

    ai clean up and fixing

--- Assets/Prefabs/BoatAI/BoatAI.cs
+++ Assets/Prefabs/BoatAI/BoatAI.cs
@@ -8,10 +8,7 @@ public class BoatAI : MonoBehaviour
 {
     public bool Alert;
     public bool ReachedDesnation;
-    public NavMeshAgent BoatNavMesh;
-    public hitBox HitBox;
     public Transform Player;
     public int TheTargetID;
     public float StopingDisteance;
-    public Transform cannon;
 }
