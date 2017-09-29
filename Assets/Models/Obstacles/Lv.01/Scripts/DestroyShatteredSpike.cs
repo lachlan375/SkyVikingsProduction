e9162ca639d614aa8f6287dd42f163c100647f78
AIE\s171623 <revanant2@gmail.com>
2017-09-29 13:30:26 +0930

    Level 1 Working changes

---
+++ Assets/Models/Obstacles/Lv.01/Scripts/DestroyShatteredSpike.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DestroyShatteredSpike : MonoBehaviour
+{
+    public float low, high;
+    public bool parent;
+
+    public ParticleSystem particle;
+
+    public void DestroyShatter()
+    {
+        particle = gameObject.GetComponent<ParticleSystem>();
+        Shader s = gameObject.GetComponent<Shader>();
+        particle.Play();
+
+        if (parent)
+            Destroy(gameObject, high);
+        else
+            Destroy(gameObject, Random.Range(low, high));
+    }
+}
---
+++ Assets/Models/Obstacles/Lv.01/Scripts/Geyser.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Collections;
+using UnityEngine.UI;
+
+public class Geyser : MonoBehaviour
+{
+    private IEnumerator coroutine1;
+
+    public float thrust = 100;
+
+    public AudioSource woosh;
+
+    private ParticleSystem particle;
+
+    void Start()
+    {
+        particle = gameObject.GetComponent<ParticleSystem>();
+        coroutine1 = Push();
+
+        if (enabled)
+        {
+            StartCoroutine(coroutine1);
+        }
+    }
+
+    IEnumerator Push()
+    {
+        while (enabled)
+        {
+            //particle.Play();
+            //woosh.Play();
+            ApplyForce();
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    void ApplyForce()
+    {
+        BoxCollider box = gameObject.GetComponent<BoxCollider>();
+        Collider[] cols = Physics.OverlapBox(transform.TransformPoint(box.center), transform.TransformVector(box.size), transform.rotation, (1 << /* ONLY Layer: */ 8));
+        foreach (Collider col in cols)
+        {
+            Rigidbody rb = col.GetComponent<Rigidbody>();
+            if (rb)
+                rb.AddForce(transform.forward * thrust);
+        }
+    }
+}
