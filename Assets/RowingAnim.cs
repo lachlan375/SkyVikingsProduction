7826eb05229966027b9574bb92374fbb71862e36
lachlan s <lachlan1066@gmail.com>
2017-10-12 18:52:35 +1030

    anim refinement

--- Assets/RowingAnim.cs
+++ Assets/RowingAnim.cs
@@ -8,7 +8,7 @@ public class RowingAnim : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		rowingRef = gameObject.GetComponent<Rowing>();	
+		
 	}
 	
 	// Update is called once per frame
