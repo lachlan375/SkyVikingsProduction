e07af939ae963bf66aeedf938aaf9e1ac1d8e179
AIE\s171138 <yerilth2@gmail.com>
2017-10-26 14:02:33 +1030

    no message

---
+++ Assets/Ice boss/Models/YetiControler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YetiControler : MonoBehaviour
+{
+    public AudioSource soundEfeact;
+    public AudioClip rawSoundEfeact;
+    public Animator YetiAini;
+    public gunContoler throwSnowbal;
+    private GameObject player;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void throwball()
+    {
+        YetiAini.SetBool("throwing", true);
+    }
+ 
+    public void finshed()
+    {
+        throwSnowbal.throwSnowball();
+        YetiAini.SetBool("throwing", false);
+    }
+    public void raw()
+    {
+        soundEfeact.PlayOneShot(rawSoundEfeact, 1.0F);
+        Debug.Log("playsound");
+    }
+}
