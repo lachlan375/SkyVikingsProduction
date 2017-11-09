efce2f429c1c4739ea73cb4aa3eed4812a0d5a1c
AIE\s171138 <yerilth2@gmail.com>
2017-11-09 14:24:43 +1030

    NEW ui  ne stat set up

--- Assets/scripits/ObjectPriceList.cs
+++ Assets/scripits/ObjectPriceList.cs
@@ -9,7 +9,19 @@ public class ObjectPriceList : MonoBehaviour
     public int CommonVaule;
     public int UncommonVale;
     public int RareVaule;
+    public int maxCargo;
+    public PlaySatSheat cargosats;
+    void Start()
+    {
+        cargosats = FindObjectOfType<PlaySatSheat>();
+         foreach (StatsBace item in cargosats.TheStats)
+        {
 
+            if (item.StatName == "maxCargo")
+            {
+                   maxCargo = item.statInt;
+            }
+        }
 
-
+    }
 }
