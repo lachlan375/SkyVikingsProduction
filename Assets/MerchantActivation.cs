bd288b291ffd0ddd9cc468d5d2b1148607a36fe1
AIE\s171545 <lachlan1066@gmail.com>
2017-11-09 15:58:43 +1030

    merchant modelling. animations

---
+++ Assets/MerchantActivation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MerchantActivation : MonoBehaviour {
+
+    public Animator merchant_anim;
+    public string animParameter_Title;
+    public bool animParameter_Value;
+    
+
+	// Use this for initialization
+	void Start () {
+        merchant_anim = gameObject.GetComponent<Animator>();
+	}
+	
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            merchant_anim.SetBool(animParameter_Title, !animParameter_Value);
+        }
+
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            merchant_anim.SetBool(animParameter_Title, animParameter_Value);
+        }
+
+    }
+
+
+
+}
