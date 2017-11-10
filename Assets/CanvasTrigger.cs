6d8b415b5365903c2b3e34e7ef5141e25bdcb638
AIE\s171545 <lachlan1066@gmail.com>
2017-11-10 14:55:01 +1030

    merchant ui stuff

---
+++ Assets/CanvasTrigger.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasTrigger : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject canvasRef;
+    public UnityEvent onAnyKeyEvent;
+
+    public Canvas merchantCanvas;
+
+    // Use this for initialization
+    void Start()
+    {
+        canvasRef = gameObject.transform.GetChild(0).Find("")
+    }
+
+    // Update is called once per frame
+
+
+
+    void Update()
+    {
+        if (Input.anyKeyDown == true)
+        {
+            if (canvasRef != null)
+            {
+                canvasRef.SetActive(false);
+            }
+            onAnyKeyEvent.Invoke();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            
+        }
+
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            
+        }
+
+    }
+
+
+
+
+}
