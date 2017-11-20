e8d86a5dfbbbac6260dc759039285147d43a51ac
Quentin <revanant2@gmail.com>
2017-11-21 03:26:12 +1030

    Prefabs added, need colliders - in process

--- Assets/Models/Environmental/Lvl_03/CloudMovement.cs
+++ Assets/Models/Environmental/Lvl_03/CloudMovement.cs
@@ -2,15 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CloudMovement : MonoBehaviour {
+public class CloudMovement : MonoBehaviour
+{
+    public float distance = 20;
+    public float speed = 3;
+    Vector3 basePos;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        basePos = transform.position;
+        Test();
+    }
+
+    void Test()
+    {
+        var pos = Random.insideUnitCircle * distance;
+        var posTranslated = new Vector3(basePos.x + pos.x, basePos.y, basePos.z + pos.y);
+        StartCoroutine(MoveTo(posTranslated));
+    }
+
+    IEnumerator MoveTo(Vector3 pos)
+    {
+        float i = 0;
+        while (i < 10 /*time*/)
+        {
+            transform.position = Vector3.Lerp(basePos, pos, i / 15);
+            yield return new WaitForSeconds(Time.deltaTime);
+            i += Time.deltaTime;
+        }
+        yield return new WaitForSeconds(1);
+        while (i > 0)
+        {
+            {
+                transform.position = Vector3.Lerp(basePos, pos, i / 15);
+                yield return new WaitForSeconds(Time.deltaTime);
+                i -= Time.deltaTime;
+            }
+        }
+        yield return new WaitForSeconds(1);
+        Test();
+    }
 }
