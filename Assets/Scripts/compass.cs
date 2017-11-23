e4be4696d5abf55f8421ec1163f12cd11544ce0f
AIE\s171138 <yerilth2@gmail.com>
2017-11-23 12:05:43 +1030

    compass

--- Assets/Scripts/compass.cs
+++ Assets/Scripts/compass.cs
@@ -5,10 +5,7 @@ using UnityEngine.UI;
 public class compass : MonoBehaviour {
 public Vector3 northDirection;
     public Transform player;
-    public Quaternion missionDirection;
     public RectTransform NorthLayer;
-    public RectTransform missionLayer;
-    public Transform missionLocation;
     // Use this for initialization
     void Start () {
         Debug.Log(gameObject);
@@ -16,22 +13,9 @@ public Vector3 northDirection;
 	
 	// Update is called once per frame
 	void Update () {
-        changeNorthDirection();
-         changemissionDirection();
-    }
-    public void changeNorthDirection()
-    {
         northDirection.z = player.eulerAngles.y;
         NorthLayer.localEulerAngles = northDirection;
-        
     }
-    public void changemissionDirection()
-    {
-        Vector3 dir = transform.position - missionLocation.position;
-        missionDirection = Quaternion.LookRotation(dir);
-        missionDirection.z = -missionDirection.y;
-        missionDirection.x = 0;
-        missionDirection.y = 0;
-        missionLayer.localRotation = missionDirection * Quaternion.Euler(northDirection);
-    }
+ 
+   
 }
